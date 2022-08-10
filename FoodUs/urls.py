b3688dd57482ffb3b55f5022eb8d311610b48e0b
ayushtamra <tamraayush@gmail.com>
2022-08-10 18:45:43 +0530

    Basic settings updated
    Signed-off-by: ayushtamra <tamraayush@gmail.com>

--- FoodUs/urls.py
+++ FoodUs/urls.py
@@ -1,7 +1,10 @@
+from django import views
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
+    path('', include('core.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls'))
 ]
