95ed3e8727ab976d7747e7c0f973ed6e003f5b89
ayushtamra <tamraayush@gmail.com>
2022-08-11 00:18:36 +0530

    Adding urls for order implementations
    Signed-off-by: ayushtamra <tamraayush@gmail.com>

--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,16 @@ from .views import *
 app_name = 'core'
 
 urlpatterns = [
-    path('', index, name="index")
+    path('', index, name="index"),
+    
+    path('restaurant/detail', views.restaurant_detail, name='restaurant_detail'),
+    path('logout_view/', views.logout_view, name='logout_view'),
+    path('restaurants/', views.restaurant_index, name='restaurant_index'),
+    path('myorders/', views.myorders, name='myorders'),
+    path('restaurant/profile', views.restaurantProfile, name='rest_profile'),
+    path('restaurant/menu/', views.menu_manipulation, name='menu'),
+    path('restaurant/orderlist/', views.orderlist, name='orderlist'),
+    path('restaurant/<int:pk>/', views.restuarantMenu, name='menu'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('checkout/pay', views.pay, name='pay'),
 ]
