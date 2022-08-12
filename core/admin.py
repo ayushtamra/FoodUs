2010ca37ffbbcefeaab7bdf59c9968c28c44b19f
ayushtamra <tamraayush@gmail.com>
2022-08-13 02:40:52 +0530

    Registered models in admin.py
    Signed-off-by: ayushtamra <tamraayush@gmail.com>

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Restaurant, FoodItem, FoodRestaurant, Offer, Order, OrderDetail, \
+    Favourite, ItemType, Payment, Cuisine
 
-# Register your models here.
+
+admin.site.register(Restaurant)
+admin.site.register(ItemType)
+admin.site.register(FoodRestaurant)
+admin.site.register(FoodItem)
+admin.site.register(OrderDetail)
+admin.site.register(Order)
+admin.site.register(Offer)
+admin.site.register(Favourite)
+admin.site.register(Cuisine)
+admin.site.register(Payment)
